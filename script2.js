5a4e9caeccaf3d83e917b4f0b4afed7ec704e117
Mohsin Reshi <mohsin_reshi@yahoo.com>
2018-03-26 12:38:48 +0530

    initial commit | create buttons

---
+++ script2.js
@@ -0,0 +1,3 @@
+const rockButton = document.querySelector('#rock').value;
+const paperButton = document.querySelector('#paper').value;
+const scissorsButton = document.querySelector('#scissors').value;
