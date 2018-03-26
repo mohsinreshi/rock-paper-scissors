c36f23a6db425cd1ba10241939d2cbcbce6c1751
Mohsin Reshi <mohsin_reshi@yahoo.com>
2018-03-26 14:41:51 +0530

    add playRound and game to make it console.log

--- script2.js
+++ script2.js
@@ -1,11 +1,16 @@
-const rockButton = document.querySelector('#rock');
-const paperButton = document.querySelector('#paper');
-const scissorsButton = document.querySelector('#scissors');
+const buttons = document.querySelectorAll('.button');
+const button = document.querySelector('.button');
 
-
-//paperButton.addEventListener('click', );
-//scissorsButton.addEventListener('click', );
-
+let playerSelection = '';
+buttons.forEach(button => button.addEventListener('click', function() {
+  playerSelection = button.value;
+  if (playerSelection == 'RELOAD') {
+    a = 0;
+    b = 0;
+  } else {
+    game();
+  }
+}));
 
 function computerPlay() {
   let randomize = Math.random();
@@ -19,12 +24,10 @@ function computerPlay() {
   }
 }
 
-let computerPlayer = computerPlay();
-let rock = rockButton.value;
-
 function playRound(playerSelection, computerSelection) {
-  if (playerSelection == computerSelection) return 'It\'s a draw!';
-
+  if (playerSelection == computerSelection) {
+    return 'It\'s a draw';
+  }
   switch (playerSelection + '|' + computerSelection) {
     case 'ROCK|PAPER':
       return 'You Lose! Paper beats Rock';
@@ -32,7 +35,37 @@ function playRound(playerSelection, computerSelection) {
     case 'ROCK|SCISSORS':
       return 'You Win! Rock beat Scissors';
       break;
-    }
+    case 'PAPER|SCISSORS':
+      return 'You Lose! Scissors beat Paper';
+      break;
+    case 'PAPER|ROCK':
+      return 'You Win! Paper beats Rock';
+      break;
+    case 'SCISSORS|PAPER':
+      return 'You Win! Scissors beat Paper';
+      break;
+    case 'SCISSORS|ROCK':
+      return 'You Lose! Rock beat Scissors';
+      break;
+  }
 }
 
-rockButton.addEventListener('click', playRound(rock, computerPlayer));
+function game() {
+  let a = 0
+  ,   b = 0
+  ,   computerSelection = computerPlay();
+    console.log(playRound(playerSelection, computerSelection));
+    if (playRound(playerSelection, computerSelection) == 'You Win! Rock beat Scissors' ||
+        playRound(playerSelection, computerSelection) == 'You Win! Paper beats Rock' ||
+        playRound(playerSelection, computerSelection) == 'You Win! Scissors beat Paper') {
+      a++;
+      console.log('Player: ' + a + ' Computer: ' + b);
+    } else if (playRound(playerSelection, computerSelection) == 'You Lose! Paper beats Rock' ||
+               playRound(playerSelection, computerSelection) == 'You Lose! Scissors beat Paper' ||
+               playRound(playerSelection, computerSelection) == 'You Lose! Rock beat Scissors') {
+      b++;
+      console.log('Player: ' + a + ' Computer: ' + b);
+    } else {
+      console.log('Player: ' + a + ' Computer: ' + b);
+    }
+  }
