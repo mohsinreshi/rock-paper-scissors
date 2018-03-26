637d3f885fd3f5b5551f287f02f4f119c1abaf15
Mohsin Reshi <mohsin_reshi@yahoo.com>
2018-03-26 13:30:46 +0530

    add initial functions

--- script2.js
+++ script2.js
@@ -1,3 +1,38 @@
-const rockButton = document.querySelector('#rock').value;
-const paperButton = document.querySelector('#paper').value;
-const scissorsButton = document.querySelector('#scissors').value;
+const rockButton = document.querySelector('#rock');
+const paperButton = document.querySelector('#paper');
+const scissorsButton = document.querySelector('#scissors');
+
+
+//paperButton.addEventListener('click', );
+//scissorsButton.addEventListener('click', );
+
+
+function computerPlay() {
+  let randomize = Math.random();
+  if (randomize >= 0 && randomize <= 0.3)
+  {
+    return 'ROCK';
+  } else if (randomize > 0.3 && randomize <= 0.7) {
+    return 'PAPER';
+  } else {
+    return 'SCISSORS';
+  }
+}
+
+let computerPlayer = computerPlay();
+let rock = rockButton.value;
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection == computerSelection) return 'It\'s a draw!';
+
+  switch (playerSelection + '|' + computerSelection) {
+    case 'ROCK|PAPER':
+      return 'You Lose! Paper beats Rock';
+      break;
+    case 'ROCK|SCISSORS':
+      return 'You Win! Rock beat Scissors';
+      break;
+    }
+}
+
+rockButton.addEventListener('click', playRound(rock, computerPlayer));
